5deaaf957a4f42c84398a0b1b807dc727871923c
Dmitrii Sukharev <47944845+sdm831@users.noreply.github.com>
2021-10-30 20:11:53 +0300

    lesson2_2_1 done

---
+++ Stepik_ASP_Core_MVC_course/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stepik_ASP_Core_MVC_course.Models
+{
+    public class Product
+    {
+        public static List<Product> products = new List<Product>()
+        {
+            new Product(01, "cement", 300, "Лучший цемент.") { },
+            new Product(02, "kraska", 800, "Яркая краска.") { },
+            new Product(03, "lak", 500, "Прозрачный лак.") { }
+        };
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Description { get; set; }
+        public Product(int id, string name, int cost, string description)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+            Description = description;
+        }
+    }
+}
