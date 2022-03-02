876530981251850b06e33c2e3ca70c614bdf4285
Dmitrii Sukharev <sdm831@gmail.com>
2022-03-02 14:52:00 +0300

    add IsDeleted(bool) in Product

--- OnlineShop.db/Migrations/20220302114241_Add_IsDeleted_Product.Designer.cs
+++ OnlineShop.db/Migrations/20220302114241_Add_IsDeleted_Product.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineShop.db;
 
 namespace OnlineShop.db.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220302114241_Add_IsDeleted_Product")]
+    partial class Add_IsDeleted_Product
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- OnlineShop.db/Models/Product.cs
+++ OnlineShop.db/Models/Product.cs
@@ -5,12 +5,19 @@ namespace OnlineShop.db.Models
 {
     public class Product
     {
-        public Guid Id { get; set; }        
+        public Guid Id { get; set; } 
+        
         public string Name { get; set; }
+
         public decimal Cost { get; set; }
+
         public string Description { get; set; }
+
         public string ImagePath { get; set; }
+
         public List<CartItem> CartItems { get; set; }
+
+        public bool IsDeleted { get; set; }
 
         public Product()
         {
